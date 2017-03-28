ea41e360de75f48a2b5549828974759ec7fb1e21
ku3nsting <ku3nsting@users.noreply.github.com>
2017-03-28 13:28:23 -0700

    added comments
    clarifies when variables within arrays within classes update their values (only on initialization, it seems)

--- classPractice.py
+++ classPractice.py
@@ -10,25 +10,83 @@
 
 import types
 
+#****************************************#
+#INITIALIZE CLASS (to hold text component)
+#****************************************#
 class Text:
-    index = None
-
-    def __init__(self, list):
-        self.list = list
+        index = 0
+        def __init__(self, array, maxIndex):
+            self.list = array
+            self.max = maxIndex
         
-#initialize the class:
-test = Text(["Hello, user! This is the 0th line",
-                     "This is the 1st line",
-                     "This is the 2nd line"])
+#****************************************#
+#INITIALIZE TEXT COMPONENTS
+#initialize all non-variable lines:
+#****************************************#
+test = Text(["Hello, user!",
+             "This is the 1st line",
+             "This is the 2nd line",
+             "This is the 3rd line"], 3)
+
+
+#****************************************#
+#****************************************#
+#BEGIN MAIN OUTPUT
+#****************************************#
+#****************************************#
+
+#Initialize global variable
+globalv = 0
 
 #change class data member "index" by assignment
 Text.index = 2
 
-#print the third item in list
-print test.list[Text.index]
+#print all data from first class instance
+for i in range(test.max + 1):
+        print globalv, test.list[i]
+        globalv = globalv + 1
 
-#change class data member "index" by assignment
-Text.index = 0
+#Change global variable to 7
+globalv = 7
 
-#print the 0th item in list
-print test.list[Text.index]
+#****************************************#
+#INITIALIZE VARIABLE-ENHANCED TEXT
+#initialize variable lines as needed:
+#necessary because variables inside class
+#data member arrays seem to have a local
+#scope, and can't be changed after they
+#are initialized
+#****************************************#
+varLine = Text(["This line contains this variable: {}".format(globalv)], 0)
+
+print varLine.list[varLine.max]
+
+#Try to change and re-print variable
+#doesn't work, despite the fact that "global" has changed outside of the class
+print "\nfirst try:"
+globalv = 9
+
+#call print function on globalv directly:
+print globalv
+
+#print it from the class:
+print varLine.list[varLine.max]
+
+#Change variable by re-initializing class
+print "\nsecond try:"
+varLine = Text(["This line contains this variable: {}".format(globalv)], 0)
+
+#call print function on globalv directly:
+print globalv
+
+#print it from the class:
+print varLine.list[varLine.max]
+
+
+
+
+
+
+
+
+
